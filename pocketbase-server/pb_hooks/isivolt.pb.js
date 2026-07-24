1f2148bb7a696ed64cc847093ec518f2b3e6a779
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-07-24 08:57:48 +0200

    feat(rc43): exponer gestión de cuentas técnicas

--- pocketbase-server/pb_hooks/isivolt.pb.js
+++ pocketbase-server/pb_hooks/isivolt.pb.js
@@ -22,3 +22,13 @@ routerAdd("POST", "/api/isivolt/movement", (e) => {
   const api = require(`${__hooks}/isivolt-utils.js`);
   return api.movement(e);
 }, $apis.requireAuth("isivolt_users"));
+
+routerAdd("GET", "/api/isivolt/technician-accounts", (e) => {
+  const api = require(`${__hooks}/technician-accounts.js`);
+  return api.list(e);
+}, $apis.requireAuth("isivolt_users"));
+
+routerAdd("POST", "/api/isivolt/technician-account", (e) => {
+  const api = require(`${__hooks}/technician-accounts.js`);
+  return api.save(e);
+}, $apis.requireAuth("isivolt_users"));

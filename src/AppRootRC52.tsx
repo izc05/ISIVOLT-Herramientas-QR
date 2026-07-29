911f2ac65c158149d761281ee95233a069949390
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-07-29 10:59:42 +0200

    feat(auth): montar piloto local de registro

--- src/AppRootRC52.tsx
+++ src/AppRootRC52.tsx
@@ -1,11 +1,13 @@
 import AppRoot from './AppRoot';
 import AdminAccessGuide from './features/auth/AdminAccessGuide';
+import LocalRegistrationPilot from './features/auth/LocalRegistrationPilot';
 
 export default function AppRootRC52() {
   return (
     <>
       <AppRoot />
       <AdminAccessGuide />
+      <LocalRegistrationPilot />
     </>
   );
 }

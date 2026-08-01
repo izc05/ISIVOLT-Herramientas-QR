c8c67a303bb7ca4676019a19bc8b7302f394cc5b
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-08-01 11:49:55 +0200

    style(v2): activar la capa responsive limpia

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './responsive.css';
 import './print.css';
 
 const root = document.getElementById('root');
